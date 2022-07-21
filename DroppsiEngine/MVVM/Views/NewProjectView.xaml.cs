5654ea0358753bf1c03586e307467d97ddcaa3f2
Droppsi <85408168+Droppsi@users.noreply.github.com>
2022-07-21 21:48:49 +0200

    made new project view almost working

--- DroppsiEngine/MVVM/Views/NewProjectView.xaml.cs
+++ DroppsiEngine/MVVM/Views/NewProjectView.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using DroppsiEngine.MVVM.ViewModels;
 
 
 namespace DroppsiEngine.MVVM.Views
@@ -19,9 +20,12 @@ namespace DroppsiEngine.MVVM.Views
             InitializeComponent();
         }
 
-        private void RadioButton_Click(object sender, RoutedEventArgs e)
-        {
+        public static readonly DependencyProperty SecondaryTextProperty = DependencyProperty.Register("SecondaryText", typeof(string), typeof(NewProjectViewModel), new FrameworkPropertyMetadata("SecondaryTextProperty"));
 
-        }
+        public string SecondaryText
+	   {
+            get { return (string)GetValue(SecondaryTextProperty); }
+            set { SetValue(SecondaryTextProperty, value); }
+	   }
     }
 }
