d20c48aaf8843d6c775c7fa61304ee06421855a4
Droppsi <85408168+Droppsi@users.noreply.github.com>
2022-07-28 00:18:24 +0200

    added a custom toolbar for editor

--- DroppsiEngineEditor/MainWindow.xaml.cs
+++ DroppsiEngineEditor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DroppsiEngineCore.Commands;
 using DroppsiEngineCore.Infrastructure.Logging;
 using OpenTK.Wpf;
 
@@ -22,10 +23,15 @@ namespace DroppsiEngineEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+	   public RelayCommand OpenFileMenu { get; set; }
 	   public MainWindow()
 	   {
 		  InitializeComponent();
-		  
+
+		  OpenFileMenu = new RelayCommand(() =>
+		  {
+
+		  });
 	   }
 
 	   private void Window_MouseDown(object sender, MouseButtonEventArgs e)
